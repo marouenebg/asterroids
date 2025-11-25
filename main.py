9a934d781d9f86e2c2c774ca047839816c366e9b
bmarouene <marouen.bougossa@gmail.com>
2025-11-25 22:14:01 +0100

    first black box

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from logger import log_state
+
+pygame.init()
+
 def main():
     print("Starting Asteroids with pygame version: "+pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while 1==1:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,"black")
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
